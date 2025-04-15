33e1587a6c9e2e68958a92ab0db66e5f67fdf550
Iris.Nyaa <118351370+IrisWidth@users.noreply.github.com>
2025-04-15 17:47:52 +0200

    Update Node.py

--- Node.py
+++ Node.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 @app.route('/save-ip', methods=['POST'])
 def save_ip():
     ip = request.json.get('ip')
-    time = datetime.datetime.now().isoformat()
-    with open('ip_log.txt', 'a') as f:
-        f.write(f"IP: {ip}, Czas: {time}\n")
-    return 'IP zapisane pomyślnie!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    if ip:
+        time = datetime.datetime.now().isoformat()
+        try:
+            with open('ip_log.txt', 'a') as f:
+                f.write(f"IP: {ip}, Czas: {time}\n")
+            return 'IP zapisane pomyślnie!'
+        except Exception as e:
+            return f'Błąd zapisu do pliku: {e}', 500
+    return 'Brak adresu IP w żądaniu!', 400
